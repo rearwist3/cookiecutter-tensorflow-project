ed7b7e2fe191816275228cb1df1ea870817b6a4d
ietheredge <rianetheredge@gmail.com>
2020-07-01 13:28:59 +0200

    initial commit

---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    name='{{ cookiecutter.repo_name  }}',
+    packages=find_packages(),
+    version='{{ cookiecutter.version }}',
+    description='{{ cookiecutter.description }}',
+    author='{{ cookiecutter.author_name }}',
+    author_email='{{ cookiecutter.email }}',
+{%- if cookiecutter.open_source_license in license_classifiers %}
+    license="{{ cookiecutter.open_source_license }}",
+{%- endif %}
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_repo }}',
+)
+
